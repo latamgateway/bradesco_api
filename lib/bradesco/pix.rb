9db9ab806bdc80a05cdea0c7fb80fea5e10ece89
SeuJota <jalvesteixeira@gmail.com>
2023-04-04 15:09:06 -0300

    Add the initial structure of the gem

---
+++ lib/bradesco/pix.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "pix/version"
+
+module Bradesco
+  module Pix
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/bradesco/pix_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Bradesco::Pix do
+  it "has a version number" do
+    expect(Bradesco::Pix::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
